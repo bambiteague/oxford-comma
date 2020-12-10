1e720752976a4d5a5c7b9de63272388a0573a9a8
bambi teague <teeegsa@gmail.com>
2020-12-09 19:55:18 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,23 +1,60 @@
-#solution 1
-def oxford_comma(array)
-       array.join("")
-end  
 
-#solution 2
-def oxford_comma(array)
-       array.join(" and ")
-end  
 
-# ^^^ Above code passes it's tests ^^^
-
-#solution 3
 def oxford_comma(array)
-    if array.size >= 3 
-      last = array.pop
-      array.push('and '+last)  # <---- need to understand this part more
-      array.join(', ')
-    else array.size >= 1
-      array.join(' and ')  
-    end  
+  array.join
+end
+
+def oxford_comma(array)
+  array.join(" and ")
+end
+
+def oxford_comma(array)
+  if array.size >= 3
+    last = array.pop
+    array.push("and "+last)
+    array.join(", ")
+  else array.size >=1
+    array.join(" and ")
   end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #solution 1
+# def oxford_comma(array)
+#        array.join("")
+# end  
+
+# #solution 2
+# def oxford_comma(array)
+#        array.join(" and ")
+# end  
+
+# # ^^^ Above code passes it's tests ^^^
+
+# #solution 3
+# def oxford_comma(array)
+#     if array.size >= 3 
+#       last = array.pop
+#       array.push('and '+last)  # <---- need to understand this part more
+#       array.join(', ')
+#     else array.size >= 1
+#       array.join(' and ')  
+#     end  
+#   end
    
